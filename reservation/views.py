242154eb58489d4afdefddb493be676ab0289a45
EGhost98 <adityachauhan8901@gmail.com>
2023-07-07 07:42:00 +0530

    Basi EndPoints Created

--- reservation/views.py
+++ reservation/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
+from rest_framework.response import Response
+from .models import Booking, Menu
+from .serializers import BookingSerializer, MenuSerializer, UserSerializer
+from rest_framework import generics, viewsets
+from rest_framework.permissions import IsAuthenticated
+from django.contrib.auth.models import User
 
-# Create your views here.
+
+class BookingviewSet(viewsets.ModelViewSet):
+    queryset = Booking.objects.all()
+    serializer_class = BookingSerializer
+
+
+class MenuItemsView(generics.ListCreateAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+
+
+class SingleMenuItemView(generics.RetrieveUpdateAPIView, generics.DestroyAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [IsAuthenticated]
